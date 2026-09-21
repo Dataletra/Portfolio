cd27e76106bb7efcda9ddda733acba98a6ef4c0b
Dataletra <dataletra@gmail.com>
2026-09-21 16:25:26 +0200

    feat: premade components for further development, added further elements to landing section

---
+++ src/app/colleagues/colleagues.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colleagues',
+  imports: [],
+  templateUrl: './colleagues.html',
+  styleUrl: './colleagues.scss',
+})
+export class Colleagues {}
--- src/app/core/core.ts
+++ src/app/core/core.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { Landing } from '../landing/landing';
+import { AboutMe } from '../about-me/about-me';
+import { Technologies } from '../technologies/technologies';
+import { FeaturedProjects } from '../featured-projects/featured-projects';
+import { Colleagues } from '../colleagues/colleagues';
+import { ContactMe } from '../contact-me/contact-me';
 
 @Component({
   selector: 'app-core',
-  imports: [Landing],
+  imports: [Landing, AboutMe, Technologies, FeaturedProjects, Colleagues, ContactMe],
   templateUrl: './core.html',
   styleUrl: './core.scss',
 })
